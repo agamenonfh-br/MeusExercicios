e7335a26f2c2afda7e72d66f9e65781434da8d19
agamenonfh-br <agamenon.fh@gmail.com>
2026-07-22 11:44:19 -0300

    Add files via upload

---
+++ 02 - ExercíciosUnidade3/ExercíciosUnidade3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 10.872395;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
